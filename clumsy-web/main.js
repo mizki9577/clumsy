525619c11b72419d62f43e6991d850bcaf78c38f
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-04-19 01:55:56 +0900

    cool

--- clumsy-web/main.js
+++ clumsy-web/main.js
@@ -1,12 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Terminal from 'terminal-in-react'
-import './index.html'
-import './style.css'
 
-const ClumsyWeb = () => (
-  <Terminal />
-)
+const PROMPT = '>>>\u00A0'
+
+class ClumsyWeb extends React.Component {
+  constructor() {
+    super()
+
+    this.prompt = React.createRef()
+    this.state = {
+      input: '',
+      outputs: [],
+    }
+  }
+
+  componentDidMount() {
+    this.prompt.current.focus()
+  }
+
+  handleChange(ev) {
+    this.setState({
+      input: ev.target.value,
+    })
+  }
+
+  handleKeyDown(ev) {
+    if (ev.key === 'Enter') {
+      const input = ev.target.value
+      this.setState({
+        input: '',
+        outputs: [...this.state.outputs, PROMPT + input, eval(input)]
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="clumsy-web">
+        <div>
+          { this.state.outputs.map((str, i) => <div key={ i }>{ str }</div>) }
+        </div>
+        <div style={{ display: 'flex' }}>
+          <span>{ PROMPT }</span>
+          <input
+            className="prompt"
+            type="text"
+            value={ this.state.input }
+            ref={ this.prompt }
+            onChange={ ev => this.handleChange(ev) }
+            onKeyDown={ ev => this.handleKeyDown(ev) }
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <ClumsyWeb />,
