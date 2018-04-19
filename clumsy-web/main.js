022e8e35b062d1e7134ef31208ac8caa62cb7e61
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-04-19 19:33:47 +0900

    web

--- clumsy-web/main.js
+++ clumsy-web/main.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import clumsy from './clumsy.rs'
 
 const PROMPT = '>>>\u00A0'
+
+const evaluate = source => {
+  const size = source.length + 1  // NOTE: non-ASCII character is not considered
+  const source_ptr = clumsy.alloc(source.length)
+  const source_array = new Int8Array(clumsy.memory.buffer, source_ptr, size)
+
+  source_array.set(Array.from(source).map(c => c.charCodeAt(0)))
+  source_array[size - 1] = 0  // terminating by null
+
+  const result_ptr = clumsy.eval(source_ptr)
+  const result_array = new Int8Array(clumsy.memory.buffer, result_ptr)
+
+  let i = 1
+  while (result_array[i] != 0) {
+    ++i
+  }
+
+  const result = String.fromCodePoint(...result_array.slice(1, i + 1))
+  return result
+}
 
 class ClumsyWeb extends React.Component {
   constructor() {
@@ -29,7 +50,7 @@ class ClumsyWeb extends React.Component {
       const input = ev.target.value
       this.setState({
         input: '',
-        outputs: [...this.state.outputs, PROMPT + input, eval(input)]
+        outputs: [...this.state.outputs, PROMPT + input, evaluate(input)]
       })
     }
   }
