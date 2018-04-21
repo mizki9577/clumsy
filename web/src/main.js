ad3b202d8d24c84fd576ab8abfd309f2bc404b49
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-04-22 00:16:08 +0900

    utf-8 compat

--- web/src/main.js
+++ web/src/main.js
@@ -5,23 +5,28 @@ import lib from './lib.rs'
 const PROMPT = '>>>\u00A0'
 
 const evaluate = source => {
-  const size = source.length + 1  // NOTE: non-ASCII character is not considered
-  const source_ptr = lib.alloc(source.length)
-  const source_array = new Int8Array(lib.memory.buffer, source_ptr, size)
+  const te = new TextEncoder()
+  const td = new TextDecoder()
 
-  source_array.set(Array.from(source).map(c => c.charCodeAt(0)))
-  source_array[size - 1] = 0  // terminating by null
+  const source_array = te.encode(source)
+  const source_size = source_array.length
 
-  const result_ptr = lib.eval(source_ptr)
-  const result_array = new Int8Array(lib.memory.buffer, result_ptr)
+  const destination_size = source_size + 1
+  const destination_ptr = lib.alloc(destination_size)
+  const destination_array = new Int8Array(lib.memory.buffer, destination_ptr, destination_size)
+  destination_array.set(source_array)
+  destination_array[destination_size - 1] = 0  // terminating by null
+
+  const result_ptr = lib.eval(destination_ptr)
+  const result_array = new Uint8Array(lib.memory.buffer, result_ptr)
 
   let i = 0
   while (result_array[i] != 0) {
     ++i
   }
-  const result = String.fromCodePoint(...result_array.slice(0, i + 1))
+  const result = td.decode(result_array.slice(0, i + 1))
 
-  lib.dealloc(source_ptr, size)
+  lib.dealloc(destination_ptr, destination_size)
   lib.free_result(result_ptr)
 
   return result
