6b00f06b08bd945e039782724fd939b5d4d6e644
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-04-28 03:34:32 +0900

    use terminal-in-react

--- web/src/ClumsyWeb.js
+++ web/src/ClumsyWeb.js
@@ -1,96 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const PROMPT = '>>>\u00A0'
-
-const Root = styled.div`
-  background: black;
-  border-radius: 0.125in;
-  border: 0.125in solid black;
-  color: white;
-  font-family: monospace;
-  height: 4.5in;
-  overflow-y: scroll;
-`
-
-const History = styled.div`
-  white-space: pre;
-`
-
-const FlexContainer = styled.div`
-  display: flex;
-`
-
-const Prompt = styled.input`
-  background: black;
-  border: none;
-  color: white;
-  font-family: inherit;
-  font-size: 100%;
-  outline: none;
-  width: 100%;
-`
+import Terminal from 'terminal-in-react'
 
 class ClumsyWeb extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.prompt = React.createRef()
-    this.state = {
-      input: '',
-      outputs: [],
-    }
-  }
-
-  componentDidMount() {
-    this.focusToPrompt()
-  }
-
-  focusToPrompt() {
-    this.prompt.current.focus()
-  }
-
-  componentDidUpdate() {
-    this.prompt.current.scrollIntoView()
-  }
-
-  handleChange(ev) {
-    this.setState({
-      input: ev.target.value,
-    })
-  }
-
-  handleKeyDown(ev) {
-    if (ev.key === 'Enter') {
-      const input = ev.target.value
-      this.setState({
-        input: '',
-        outputs: [
-          ...this.state.outputs,
-          PROMPT + input,
-          this.props.wasm.evaluate(input),
-        ],
-      })
-    }
+  handleInput(input) {
+    return this.props.wasm.evaluate(input)
   }
 
   render() {
     return (
-      <Root onClick={() => this.focusToPrompt()}>
-        <History>
-          {this.state.outputs.map((str, i) => <div key={i}>{str}</div>)}
-        </History>
-        <FlexContainer>
-          <span>{PROMPT}</span>
-          <Prompt
-            innerRef={this.prompt}
-            type="text"
-            value={this.state.input}
-            onChange={ev => this.handleChange(ev)}
-            onKeyDown={ev => this.handleKeyDown(ev)}
-          />
-        </FlexContainer>
-      </Root>
+      <Terminal
+        color="white"
+        prompt="white"
+        startState="maximised"
+        hideTopBar={true}
+        allowTabs={false}
+        style={{ fontFamily: 'monospace', fontSize: 'large' }}
+        commandPassThrough={input => this.handleInput(input.join(' '))}
+      />
     )
   }
 }
