fc97b3b1f25c2cd95b1c478eb2b436b6e25a312b
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-02 00:55:10 +0900

    I should use to_owned as far as possible

--- src/translator/tests.rs
+++ src/translator/tests.rs
@@ -6,7 +6,7 @@ fn translate_abstraction() {
     let a = DeBruijnIndex::from_ast(&new_abstraction("x", new_variable("x")));
     let expected = DeBruijnIndex::Abstraction(box DeBruijnIndex::Variable {
         index: Some(0),
-        name: "x".to_string(),
+        name: "x".to_owned(),
     });
     assert_eq!(expected, a);
 }
