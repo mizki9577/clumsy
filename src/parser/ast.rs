09aa66383728b4ed27d3606db3dec1f3d4ac4094
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-06 00:59:50 +0900

    remove utils

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -14,6 +14,26 @@ pub enum Expression {
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Variable(pub String);
 
+impl Expression {
+    pub fn new_abstraction(parameter: &str, expression: Expression) -> Expression {
+        Expression::Abstraction {
+            parameter: Variable::new(parameter),
+            expression: box expression,
+        }
+    }
+
+    pub fn new_application(callee: Expression, argument: Expression) -> Expression {
+        Expression::Application {
+            callee: box callee,
+            argument: box argument,
+        }
+    }
+
+    pub fn new_variable(variable: &str) -> Expression {
+        Expression::Variable(Variable::new(variable))
+    }
+}
+
 impl Variable {
     pub fn new(variable: &str) -> Variable {
         Variable(variable.to_owned())
--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -1,14 +1,13 @@
+use self::ast::*;
 use super::*;
-use lexer::Lexer;
-use utils::*;
 
 #[test]
 fn it_can_parse_abstraction() {
     let a = parse_abstraction(&mut Lexer::new("\\x y. x").peekable());
     let b = parse_abstraction(&mut Lexer::new("\\x. \\y. x").peekable());
-    let expected = Ok(new_abstraction(
+    let expected = Ok(Expression::new_abstraction(
         "x",
-        new_abstraction("y", new_variable("x")),
+        Expression::new_abstraction("y", Expression::new_variable("x")),
     ));
     assert_eq!(expected, a);
     assert_eq!(expected, b);
@@ -18,9 +17,9 @@ fn it_can_parse_abstraction() {
 fn it_can_parse_application() {
     let a = parse_application(&mut Lexer::new("x y z").peekable());
     let b = parse_application(&mut Lexer::new("((x) y) z").peekable());
-    let expected = Ok(new_application(
-        new_application(new_variable("x"), new_variable("y")),
-        new_variable("z"),
+    let expected = Ok(Expression::new_application(
+        Expression::new_application(Expression::new_variable("x"), Expression::new_variable("y")),
+        Expression::new_variable("z"),
     ));
     assert_eq!(expected, a);
     assert_eq!(expected, b);
