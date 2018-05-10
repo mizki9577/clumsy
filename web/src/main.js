030c8a524054fe46a5d0b4c72d1a85f45c4fb1b8
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-11 04:10:13 +0900

    coolweb

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ClumsyWeb from './ClumsyWeb.js'
 
-import('./clumsy_web.js').then(wasm => {
-  ReactDOM.render(<ClumsyWeb wasm={wasm} />, document.querySelector('main'))
-})
+ReactDOM.render(<ClumsyWeb />, document.querySelector('main'))
 
 // vim: set ts=2 sw=2 et:
