3c099e3cdf088f10ad895e98848e4f8f5137e131
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-14 01:39:24 +0900

    I AM SAD

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,20 +3,20 @@ mod tests;
 
 pub mod ast;
 
-use lexer::{Token, TokenType};
+use lexer::{Lexer, TokenType};
+use std::iter::Peekable;
 use std::result;
 
 type Result<T> = result::Result<T, String>;
 
-fn expect(tokens: &mut impl Iterator<Item = Token>, expected: TokenType) -> Result<()> {
+fn expect(tokens: &mut Peekable<Lexer>, expected: &TokenType) -> Result<()> {
     match tokens.next().unwrap() {
-        ref found if expected == found.token_type => Ok(()),
+        ref found if expected == &found.token_type => Ok(()),
         found => Err(format!("Expected {}, found {}", expected, found)),
     }
 }
 
-pub fn parse_expression(tokens: &mut impl Iterator<Item = Token>) -> Result<ast::Expression> {
-    let tokens = &mut tokens.peekable();
+pub fn parse_expression(tokens: &mut Peekable<Lexer>) -> Result<ast::Expression> {
     let token = tokens.peek().unwrap();
     match token.token_type {
         TokenType::Lambda => parse_abstraction(tokens),
@@ -25,18 +25,17 @@ pub fn parse_expression(tokens: &mut impl Iterator<Item = Token>) -> Result<ast:
     }
 }
 
-fn parse_abstraction(tokens: &mut impl Iterator<Item = Token>) -> Result<ast::Expression> {
-    expect(tokens, TokenType::Lambda)?;
+fn parse_abstraction(tokens: &mut Peekable<Lexer>) -> Result<ast::Expression> {
+    expect(tokens, &TokenType::Lambda)?;
     let parameters = parse_parameters(tokens)?;
-    expect(tokens, TokenType::Dot)?;
+    expect(tokens, &TokenType::Dot)?;
     let expression = parse_expression(tokens)?;
     Ok(ast::Expression::Abstraction(ast::Abstraction::new(
         parameters, expression,
     )))
 }
 
-fn parse_parameters(tokens: &mut impl Iterator<Item = Token>) -> Result<Vec<ast::Variable>> {
-    let tokens = &mut tokens.peekable();
+fn parse_parameters(tokens: &mut Peekable<Lexer>) -> Result<Vec<ast::Variable>> {
     let mut parameters = vec![parse_variable(tokens)?];
     while let TokenType::Variable(_) = tokens.peek().unwrap().token_type {
         parameters.push(parse_variable(tokens)?);
@@ -44,32 +43,27 @@ fn parse_parameters(tokens: &mut impl Iterator<Item = Token>) -> Result<Vec<ast:
     Ok(parameters)
 }
 
-fn parse_application(tokens: &mut impl Iterator<Item = Token>) -> Result<ast::Expression> {
-    let tokens = &mut tokens.peekable();
+fn parse_application(tokens: &mut Peekable<Lexer>) -> Result<ast::Expression> {
     let mut expressions = Vec::new();
-    loop {
-        if let Some(token) = tokens.peek() {
-            expressions.push(match token.token_type {
-                TokenType::Variable(_) => ast::Expression::Variable(parse_variable(tokens)?),
-                TokenType::LeftBracket => {
-                    expect(tokens, TokenType::LeftBracket)?;
-                    let expression = parse_expression(tokens)?;
-                    expect(tokens, TokenType::RightBracket)?;
-                    expression
-                }
-                TokenType::Lambda => parse_abstraction(tokens)?,
-                _ => break,
-            });
-        } else {
-            break;
-        }
+    while let Some(token) = tokens.peek() {
+        expressions.push(match token.token_type {
+            TokenType::Variable(_) => ast::Expression::Variable(parse_variable(tokens)?),
+            TokenType::LeftBracket => {
+                expect(tokens, &TokenType::LeftBracket)?;
+                let expression = parse_expression(tokens)?;
+                expect(tokens, &TokenType::RightBracket)?;
+                expression
+            }
+            TokenType::Lambda => parse_abstraction(tokens)?,
+            _ => break,
+        });
     }
     Ok(ast::Expression::Application(ast::Application::new(
         expressions,
     )))
 }
 
-fn parse_variable(tokens: &mut impl Iterator<Item = Token>) -> Result<ast::Variable> {
+fn parse_variable(tokens: &mut Peekable<Lexer>) -> Result<ast::Variable> {
     let token = tokens.next().unwrap();
     match token.token_type {
         TokenType::Variable(variable) => Ok(ast::Variable::from(variable)),
