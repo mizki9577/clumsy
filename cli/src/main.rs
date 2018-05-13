86e61c87145c386b8cccd041c849db53aed6d644
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-14 01:39:24 +0900

    I AM SAD

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -33,7 +33,7 @@ fn main() {
 }
 
 fn eval(source: &str) -> Result<String, String> {
-    let mut tokens = Lexer::new(source).peekable();
-    let ast = parser::parse_expression(&mut tokens)?;
+    let ref mut tokens = Lexer::new(source).peekable();
+    let ast = parser::parse_expression(tokens)?;
     Ok(format!("{}", Expression::from(&ast)))
 }
