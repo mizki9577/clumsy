6f5984b28c3db0ea7d8dbdd0fcbf17127d787c1c
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-16 00:59:45 +0900

    move tests

--- src/interpreter/expression.rs
+++ src/interpreter/expression.rs
@@ -169,6 +169,32 @@ mod test {
     use super::*;
 
     #[test]
+    fn translate_abstraction() {
+        let a = Expression::from(&AST::Abstraction(ASTAbstraction::new(vec!["x", "x"], "x")));
+        let expected = Expression::Abstraction(Abstraction::new(
+            "x",
+            Expression::Abstraction(Abstraction::new(
+                "x",
+                Expression::Variable(Variable::new(Some(0), "x")),
+            )),
+        ));
+        assert_eq!(expected, a);
+    }
+
+    #[test]
+    fn translate_application() {
+        let a = Expression::from(&AST::Application(ASTApplication::new(vec!["a", "b", "c"])));
+        let expected = Expression::Application(Application::new(
+            Expression::Application(Application::new(
+                Expression::Variable(Variable::new(None, "a")),
+                Expression::Variable(Variable::new(None, "b")),
+            )),
+            Expression::Variable(Variable::new(None, "c")),
+        ));
+        assert_eq!(expected, a);
+    }
+
+    #[test]
     fn test_shift() {
         let expected = Expression::Variable(Variable::new(Some(1), "x"));
         let result = Expression::Variable(Variable::new(Some(0), "x")).shift_impl(true, 0);
