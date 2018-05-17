1771f04fc5717e7d4ea259b48d90df7c01b320b0
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-17 17:16:12 +0900

    improve command line interface

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,7 @@
+#![feature(underscore_imports)]
+
+#[macro_use]
+extern crate structopt;
 extern crate ansi_term;
 extern crate clumsy;
 extern crate rustyline;
@@ -7,32 +11,86 @@ use clumsy::interpreter::Expression;
 use clumsy::lexer::Lexer;
 use clumsy::parser;
 use rustyline::error::ReadlineError;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read as _;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 static PROMPT: &str = ">>> ";
 static RED: Color = Color::Fixed(9);
 
+#[derive(StructOpt)]
+#[structopt(name = "Clumsy")]
+struct Options {
+    #[structopt(value_name = "file", parse(from_os_str), conflicts_with = "expression")]
+    program: Option<PathBuf>,
+
+    #[structopt(short = "e", value_name = "expression", conflicts_with = "program")]
+    expression: Option<String>,
+
+    #[structopt(long = "history", value_name = "file", env = "CLUMSY_HISTORY", parse(from_os_str))]
+    history: Option<PathBuf>,
+}
+
 fn main() {
+    match Options::from_args() {
+        Options {
+            expression: Some(ref expression),
+            ..
+        } => eval(expression),
+
+        Options {
+            program: Some(path),
+            ..
+        } => {
+            match File::open(path) {
+                Ok(file) => {
+                    let mut reader = BufReader::new(file);
+                    let mut source = String::new();
+                    if reader.read_to_string(&mut source).is_ok() {
+                        eval(&source);
+                    } else {
+                        // TODO: handle errors
+                    }
+                }
+                Err(error) => println!("{}", error),
+            }
+        }
+
+        Options { ref history, .. } => repl(history),
+    }
+}
+
+fn repl(history: &Option<PathBuf>) {
     let mut rl = rustyline::Editor::<()>::new().history_ignore_dups(true);
+    if let Some(history) = history {
+        let _ = rl.load_history(history);
+    }
+
     loop {
         match rl.readline(PROMPT) {
             Ok(source) => {
                 rl.add_history_entry(&source);
-                let result = eval(&source);
-                match result {
-                    Ok(result) => println!("{}", result),
-                    Err(error) => println!("{}", RED.paint(error)),
-                }
+                eval(&source);
             }
             Err(ReadlineError::Eof) => break,
             Err(error) => println!("{}", RED.paint(error.to_string())),
         }
     }
+
+    if let Some(history) = history {
+        let _ = rl.save_history(history);
+    }
 }
 
-fn eval(source: &str) -> Result<String, String> {
-    let ref mut tokens = Lexer::new(source).peekable();
-    let ast = parser::parse_expression(tokens)?;
-    let expression = Expression::from(&ast);
-    let result = expression.evaluate();
-    Ok(format!("{}", result))
+fn eval(source: &str) {
+    let tokens = &mut Lexer::new(source).peekable();
+    match parser::parse_expression(tokens).map(|ast| {
+        let expression = Expression::from(&ast);
+        expression.evaluate()
+    }) {
+        Ok(result) => println!("{}", result),
+        Err(error) => println!("{}", RED.paint(error)),
+    };
 }
