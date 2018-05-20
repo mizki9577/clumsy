f0ced655424af65a7055e4d374023ca53aa5a658
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-05-20 21:52:17 +0900

    move parser tests

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,3 @@
-#[cfg(test)]
-mod tests;
-
 pub mod ast;
 
 use self::ast::*;
@@ -81,4 +78,27 @@ fn parse_identifier(tokens: &mut Peekable<Lexer>) -> Result<ASTIdentifier> {
 
 fn parse_let(tokens: &mut Peekable<Lexer>) -> Result<ASTLet> {
     unimplemented!()
+}
+
+#[test]
+fn test_parse_abstraction() {
+    let result = parse_abstraction(&mut Lexer::new("\\x y. x").peekable());
+    let expected = Ok(ASTExpression::Abstraction(ASTAbstraction::new(
+        vec![ASTIdentifier::from("x"), ASTIdentifier::from("y")],
+        ASTExpression::Application(ASTApplication::new(vec![ASTExpression::Identifier(
+            ASTIdentifier::from("x"),
+        )])),
+    )));
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn test_parse_application() {
+    let result = parse_application(&mut Lexer::new("x y z").peekable());
+    let expected = Ok(ASTExpression::Application(ASTApplication::new(vec![
+        ASTExpression::Identifier(ASTIdentifier::from("x")),
+        ASTExpression::Identifier(ASTIdentifier::from("y")),
+        ASTExpression::Identifier(ASTIdentifier::from("z")),
+    ])));
+    assert_eq!(expected, result);
 }
