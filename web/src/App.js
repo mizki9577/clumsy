e620ea3cc37a3cc86f2cd643e0a29afc8f5202ec
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-06-10 05:57:17 +0900

    foo

--- web/src/App.js
+++ web/src/App.js
@@ -17,13 +17,34 @@ const styles = () => ({
   },
 })
 
+const initial_source = `let 1 = \\f x. f x;
+let 3 = \\f x. f (f (f x));
+let 6 = \\f x. f (f (f (f (f (f x)))));
+
+let mul = \\m n f. m (n f);
+let pred = \\n f x. n (\\g h. h (g f)) (\\u. x) (\\u. u);
+let sub = \\m n. n pred m;
+
+let true = \\x y. x;
+let false = \\x y. y;
+let and = \\p q. p q p;
+let is_zero = \\n. n (\\x. false) true;
+let is_equal = \\m n. and (is_zero (sub m n)) (is_zero (sub n m));
+
+let Z = \\f. (\\x. f (\\y. x x y)) (\\x. f (\\y. x x y));
+
+let factorial_impl = \\f n. (is_zero n) 1 (mul n (f (pred n)));
+let factorial = Z factorial_impl;
+
+is_equal (factorial 3) 6;`
+
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      source: localStorage.getItem('source') || '',
-      keyboardHandler: localStorage.getItem('keyboardHandler'),
+      source: localStorage.getItem('clumsy.source') || initial_source,
+      keyboardHandler: localStorage.getItem('clumsy.keyboardHandler'),
       ready: false,
       result: null,
     }
@@ -38,12 +59,12 @@ class App extends React.Component {
 
   handleConfigChange(state) {
     this.setState({ keyboardHandler: state.keyboardHandler })
-    localStorage.setItem('keyboardHandler', state.keyboardHandler)
+    localStorage.setItem('clumsy.keyboardHandler', state.keyboardHandler)
   }
 
   handleEditorUpdate(source) {
     this.setState({ source })
-    localStorage.setItem('source', source)
+    localStorage.setItem('clumsy.source', source)
   }
 
   handleEvaluation() {
