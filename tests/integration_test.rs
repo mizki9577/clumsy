07431e99892fb6dd922c254c907136f627702a8c
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-06-10 03:03:59 +0900

    no eager.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,12 +1,12 @@
 extern crate clumsy;
 
 #[test]
-fn test_eager() {
+fn test_factorial_of_3_is_equal_to_6() {
     let source = include_str!("factorial.clumsy");
     println!(
         "{}",
         clumsy::interpreter::Expression::from(
             &clumsy::parser::parse(&mut clumsy::lexer::Lexer::new(source).peekable()).unwrap()
-        ).evaluate_eager()
+        ).evaluate()
     );
 }
