140752b4231ea4e33c0b5198c4b0b26499ca2756
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2018-08-05 00:25:01 +0900

    add a test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -10,3 +10,14 @@ fn test_factorial_of_3_is_equal_to_6() {
         ).evaluate()
     );
 }
+
+#[test]
+fn test_6th_fibonacci_number_is_equal_to_8() {
+    let source = include_str!("fibonacci.clumsy");
+    println!(
+        "{}",
+        clumsy::ast::Expression::from_cst_program(
+            &clumsy::parser::parse(&mut clumsy::lexer::Lexer::new(source)).unwrap()
+        ).evaluate()
+    );
+}
